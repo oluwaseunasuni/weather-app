22b5e0e061f4e2766e64b659b47458bd2db0d6cf
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-04-07 14:27:24 +0100

    removed white spaces

--- JS/main.js
+++ JS/main.js
@@ -3,16 +3,13 @@ window.addEventListener ('load', ()=> {
     let lat;
     let temperatureDegree = document.querySelector('.temperature-degree');
     let temperatureDescription = document.querySelector('.temperature-description');
-    let locationTimezone = document.querySelector('.location-timezone')
-
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition
         (position =>{
             lon = position.coords.longitude;
             lat = position.coords.latitude;
-
-
 
             // const proxy = 'http://cors-anywhere.herokuapp.com/'
             const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&APPID=a05bc7d3dd3a3be43342c46843837be0`;
@@ -23,16 +20,13 @@ window.addEventListener ('load', ()=> {
                 })
                 .then(data => {
                     console.log(data);
-                    // const temperature = data['current']['temp'];
+                    const temperature = data['current']['temp'];
                     const summary = data ['current']['weather'][0]['description'];
                     // const location = data['timezone'];
-                    temperatureDegree.textContent = data.current.temp;
+                    temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     locationTimezone.textContent = data.timezone;
-                }) 
-        });
-
-            
-    } 
-
+                })
+        }); 
+    }
 });
